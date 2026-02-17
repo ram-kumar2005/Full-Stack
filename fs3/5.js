035f7fa629adc79a23508c3384c7f15afa595b8e
Ram <ramkumarnathan55@gmail.com>
2026-02-17 14:31:03 +0530

    Add files via upload

---
+++ fs3/5.js
@@ -0,0 +1,10 @@
+
+<script>
+function comp(str1, str2) {
+    return str1.length === str2.length;
+}
+
+console.log(comp("AB","CD"));   // true
+console.log(comp("ABC","DE"));  // false
+</script>
+
